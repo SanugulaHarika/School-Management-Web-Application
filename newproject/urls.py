9786e2e7b07dd1b5f88617b36aed1007b17b2950
SanugulaHarika <sanugulaharika81@gmail.com>
2026-03-06 14:40:41 +0530

    Update urls.py

--- newproject/urls.py
+++ newproject/urls.py
@@ -22,6 +22,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    #path("",index),
+    path("",index),
     path('',include("newapp.urls")),
 ]+ static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+
