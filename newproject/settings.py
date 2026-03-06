f2d51b063b7b49a3e99fc0fd8b894625bb5564fa
SanugulaHarika <sanugulaharika81@gmail.com>
2026-03-06 14:57:51 +0530

    Update settings.py

--- newproject/settings.py
+++ newproject/settings.py
@@ -119,6 +119,8 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+
 
